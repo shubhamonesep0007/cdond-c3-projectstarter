44511b2882db58fd60a0d0fcb6f5791ff1b89fa5
shubhamonesep0007 <96438961+shubhamonesep0007@users.noreply.github.com>
2023-04-18 11:02:56 +0530

    conigure infra -12

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIE=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=database-1.ccuqg3uoxp0p.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres01
+export TYPEORM_DATABASE=postgres
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=src/migrations/
